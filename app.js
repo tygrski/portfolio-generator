3e8e451aeffed14d50bf9dca4f2a7e25841ed598
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-28 17:59:30 -0500

    generating page

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
 
 const[name, github] = profileDataArgs;
 
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if(err) throw new Error(err);
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
-console.log(name,github);
-console.log(generatePage(name, github));
