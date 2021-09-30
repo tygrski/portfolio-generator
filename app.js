c3fc051fee5fb08de504b7fc08801c2c36c7e5bc
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-30 16:41:23 -0500

    validation req answers

--- app.js
+++ app.js
@@ -16,12 +16,28 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'what is your name?'
+      message: 'what is your name?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+         console.log('Please enter your name!') 
+         return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+         console.log('Please enter your name!') 
+         return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -48,12 +64,28 @@ const promptUser = () => {
       {
         type: 'input',
         name:'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+           console.log('Please enter your name!') 
+           return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Proveide a description of the project(Required)'
+        message: 'Proveide a description of the project(Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+           console.log('Please enter your name!') 
+           return false;
+          }
+        }
       },
       {
         type: 'checkbox',
@@ -64,7 +96,15 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+           console.log('Please enter your name!') 
+           return false;
+          }
+        }
       },
       {
         type: 'confirm',
