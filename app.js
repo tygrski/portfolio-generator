739c4ea21fd7a48a8dbc612c242c805972706b7c
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-04 13:47:07 -0500

    finished

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
-const inquirer = require('inquirer')
-const fs = require('fs');
+const { writeFile, copyFile }= require('./utils/generate-site.js')
+const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
-
   return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'what is your name?',
+      message: 'What is your name? (Required)',
       validate: nameInput => {
         if (nameInput) {
           return true;
         } else {
-         console.log('Please enter your name!') 
-         return false;
+          console.log('Please enter your name!');
+          return false;
         }
       }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username',
-      validate: nameInput => {
-        if (nameInput) {
+      message: 'Enter your GitHub Username (Required)',
+      validate: githubInput => {
+        if (githubInput) {
           return true;
         } else {
-         console.log('Please enter your name!') 
-         return false;
+          console.log('Please enter your GitHub username!');
+          return false;
         }
       }
     },
@@ -48,87 +40,80 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'about',
-      message: ' Provide some information about yourself.',
-      when: ({ confirmAbout }) => {
-        if (confirmAbout) {
-          return true;
-        } else {
-          return false;
-        }
-      }
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => confirmAbout
     }
   ]);
 };
 
+const promptProject = portfolioData => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
 
- const promptProject = portfolioData => {
-  
-  //  if there's no 'projects' array property, create one
+  // If there's no 'projects' array property, create one
   if (!portfolioData.projects) {
     portfolioData.projects = [];
   }
-  
-  console.log(`
-   =================
-   Add a New Project
-   =================
-   `);
-    return inquirer.prompt([
+  return inquirer
+    .prompt([
       {
         type: 'input',
-        name:'name',
-        message: 'What is the name of your project?',
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
         validate: nameInput => {
           if (nameInput) {
             return true;
           } else {
-           console.log('Please enter your name!') 
-           return false;
+            console.log('You need to enter a project name!');
+            return false;
           }
         }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Proveide a description of the project(Required)',
-        validate: nameInput => {
-          if (nameInput) {
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
             return true;
           } else {
-           console.log('Please enter your name!') 
-           return false;
+            console.log('You need to enter a project description!');
+            return false;
           }
         }
       },
       {
         type: 'checkbox',
         name: 'languages',
-        message: 'What did you build this project with?(Check all that apply',
-        choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'JQuery', 'Bootstrap', 'Node']
+        message: 'What did you this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
       },
       {
         type: 'input',
         name: 'link',
         message: 'Enter the GitHub link to your project. (Required)',
-        validate: nameInput => {
-          if (nameInput) {
+        validate: linkInput => {
+          if (linkInput) {
             return true;
           } else {
-           console.log('Please enter your name!') 
-           return false;
+            console.log('You need to enter a project GitHub link!');
+            return false;
           }
         }
       },
       {
         type: 'confirm',
         name: 'feature',
-        message: 'Would to feature this project?',
+        message: 'Would you like to feature this project?',
         default: false
       },
       {
         type: 'confirm',
         name: 'confirmAddProject',
-        message: ' Would you like to enter another project?',
+        message: 'Would you like to enter another project?',
         default: false
       }
     ])
@@ -140,18 +125,48 @@ const promptUser = () => {
         return portfolioData;
       }
     });
- };
+};
 
- promptUser()
+promptUser()
   .then(promptProject)
   .then(portfolioData => {
-   const pageHTML = generatePage(portfolioData);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Page created! Check out index.html in this directory to see it!');
-});
-
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
+
+
+
+ //   .then(promptProject)
+//   .then(portfolioData => {
+//    const pageHTML = generatePage(portfolioData);
+
+//    fs.writeFile('./dist/index.html', pageHTML, err => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log('Page created! Check out index.html in this directory to see it!');
+  
+//     fs.copyFile('./src/style.css', './dist/style.css', err => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log('Style sheet copied successfully!');
+//     });
+//   });
+// });
  
